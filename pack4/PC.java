a4fd77dc1e19a2c2dac36ff111b405644ffbab6d
TaylorSwift_ <65851608+kddai@users.noreply.github.com>
2020-10-13 18:11:18 +0800

    Add files via upload

--- pack4/PC.java
+++ pack4/PC.java
@@ -8,11 +8,15 @@ public class PC {
 	public PC(){
 		
 	}
-	public void setCpu(CPU cpu){
-		this.cpu=cpu;                           //赋值给CPU
+	public PC(CPU cpu,Harddisk HD) {
+		this.cpu = cpu;
+		this.HD = HD;
 	}
-	public void setHarddisk(Harddisk HD){
-		this.HD=HD;                             //赋值给HD
+	public void setCpu(CPU c){
+		this.cpu=c;                           //赋值给CPU
+	}
+	public void setHarddisk(Harddisk h){
+		this.HD=h;                             //赋值给HD
 	}
 	public void show(){                                     //show()
 		System.out.println("cpu速度为："+cpu.getSpeed());
